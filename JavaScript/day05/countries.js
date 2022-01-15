2599362209cfb0997d51f5af9200dbacb44c19e0
marucube35 <marucube35@gmail.com>
2022-01-15 10:37:20 +0700

    update

--- JavaScript/day05/countries.js
+++ JavaScript/day05/countries.js
@@ -12,23 +12,25 @@ const countries = [
   'Kenya'
 ]
 
-if(countries.includes('Ethiopia') === false)
+if (countries.includes('Ethiopia') === false)
   countries.push('Ethiopia')
 else
   console.log('ethiopia'.toUpperCase())
 
-console.log(countries.slice(0,9))
+console.log(countries.slice(0, 10))
 
 let len = countries.length
-if(len % 2 === 0)
-{
-    let middleOne = countries[Math.floor((len - 1)/2)]
-    let middleTwo = countries[Math.ceil((len - 1)/2)]
-    console.log('middle items: ',middleOne,middleTwo)
+let middle = null
+let subArr1 = []
+let subArr2 = []
+if (len % 2 === 0) {
+  middle = Math.floor((len - 1) / 2)
 }
-else
-{
-    let middle = countries[(len - 1)/2]
-    console.log('middle item: ',middle)
+else {
+  middle = (len - 1) / 2
 }
+subArr1 = countries.slice(0, middle)
+subArr2 = countries.slice(middle + 1,len)
+console.log(subArr1)
+console.log(subArr2)
 
