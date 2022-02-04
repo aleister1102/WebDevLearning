0ffc04c7262f47d8db785155c5ca42edbad67a2d
marucube35 <marucube35@gmail.com>
2022-02-04 15:45:49 +0700

    update css selector

--- main.js
+++ main.js
@@ -1,7 +1,2 @@
-const score = [700, 8, 3, 10, 9];
-console.log(
-    score.sort(function compareNumbers(firstEl, secondEl) {
-        return firstEl - secondEl;
-    }
-    )
-);
+var element = document.querySelector("#box").style.color = "#F05123"
+console.log([element])
