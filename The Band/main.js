79ef616ace9abe2aaa6f297e8256e15fb25196e5
marucube35 <marucube35@gmail.com>
2022-02-14 17:10:32 +0700

    done modal

---
+++ The Band/main.js
@@ -0,0 +1,28 @@
+let modal = document.querySelector(".js-modal");
+let modalContainer = document.querySelector(".js-modal-container")
+
+// Open modal
+let buyTickets = document.querySelectorAll('.js-buy-ticket');
+for (let element of buyTickets) {
+    element.addEventListener('click', function () {
+        modal.classList.add("modal--open")
+    })
+}
+
+function closeModal() {
+    modal.classList.remove("modal--open")
+}
+
+// Close modal with button
+let closeButtonModal = document.querySelectorAll(".js-close-modal");
+for (let element of closeButtonModal) {
+    element.addEventListener('click', closeModal)
+}
+
+// Close modal without button
+modal.addEventListener('click', closeModal)
+
+// Prevent Propagation
+modalContainer.addEventListener('click', function (event) {
+    event.stopPropagation();
+})
